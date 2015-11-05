10b5ca80c5ed817ae16608fc5175acbf326d63f1
Daniel Grayling <daniel.grayling@hp.com>
2015-11-05 16:14:45 +0000

    [CCUK-3450] The fieldname parameter can be sent as null, so the parametric request replaces the field names with the empty set to prevent null pointer exceptions. [rev: alex.scown]

--- src/main/java/com/hp/autonomy/parametricvalues/ParametricRequest.java
+++ src/main/java/com/hp/autonomy/parametricvalues/ParametricRequest.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
+import java.util.Collections;
 import java.util.Set;
 
 @Data
@@ -32,7 +33,7 @@ public class ParametricRequest implements Serializable {
         this.databases = databases;
         this.query = query;
         this.fieldText = fieldText;
-        this.fieldNames = fieldNames;
+        this.fieldNames = fieldNames == null ? Collections.<String>emptySet() : fieldNames;
     }
 
     @JsonPOJOBuilder(withPrefix = "set")
