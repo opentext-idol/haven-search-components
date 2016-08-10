7b2e3d7318481e9475bc7eee689140ce33191502
Rosie Taylor <rosie.taylor@hpe.com>
2016-08-10 14:35:13 +0100

    [FIND-241, FIND-108] Manage HoD warnings returned on all query responses [rev: ivo.miller]

--- hod/src/test/java/com/hp/autonomy/searchcomponents/hod/search/HodDocumentServiceTest.java
+++ hod/src/test/java/com/hp/autonomy/searchcomponents/hod/search/HodDocumentServiceTest.java
@@ -12,10 +12,12 @@ import com.hp.autonomy.hod.client.api.textindex.query.content.GetContentService;
 import com.hp.autonomy.hod.client.api.textindex.query.search.FindSimilarService;
 import com.hp.autonomy.hod.client.api.textindex.query.search.Print;
 import com.hp.autonomy.hod.client.api.textindex.query.search.QueryRequestBuilder;
+import com.hp.autonomy.hod.client.api.textindex.query.search.QueryResults;
 import com.hp.autonomy.hod.client.api.textindex.query.search.QueryTextIndexService;
 import com.hp.autonomy.hod.client.api.textindex.query.search.Sort;
 import com.hp.autonomy.hod.client.api.textindex.query.search.Summary;
 import com.hp.autonomy.hod.client.error.HodErrorException;
+import com.hp.autonomy.hod.client.warning.HodWarning;
 import com.hp.autonomy.hod.sso.HodAuthenticationPrincipal;
 import com.hp.autonomy.searchcomponents.core.authentication.AuthenticationInformationRetriever;
 import com.hp.autonomy.searchcomponents.core.search.GetContentRequest;
@@ -94,7 +96,7 @@ public class HodDocumentServiceTest {
 
     @Test
     public void queryTextIndexModified() throws HodErrorException {
-        final Documents<HodSearchResult> mockedResults = mockResults();
+        final QueryResults<HodSearchResult> mockedResults = mockResults();
         when(queryTextIndexService.queryTextIndexWithText(anyString(), any(QueryRequestBuilder.class))).thenReturn(mockedResults);
 
         final QueryRestrictions<ResourceIdentifier> queryRestrictions = testUtils.buildQueryRestrictions();
@@ -116,7 +118,7 @@ public class HodDocumentServiceTest {
 
     @Test
     public void queryTextIndexRaw() throws HodErrorException {
-        final Documents<HodSearchResult> mockedResults = mockResults();
+        final QueryResults<HodSearchResult> mockedResults = mockResults();
         when(queryTextIndexService.queryTextIndexWithText(anyString(), argThat(new HasPropertyWithValue<>("queryProfile", nullValue())))).thenReturn(mockedResults);
 
         final QueryRestrictions<ResourceIdentifier> queryRestrictions = testUtils.buildQueryRestrictions();
@@ -138,7 +140,7 @@ public class HodDocumentServiceTest {
 
     @Test
     public void queryTextIndexForPromotions() throws HodErrorException {
-        final Documents<HodSearchResult> mockedResults = mockResults();
+        final QueryResults<HodSearchResult> mockedResults = mockResults();
         when(queryTextIndexService.queryTextIndexWithText(anyString(), argThat(new HasPropertyWithValue<>("promotions", is(true))))).thenReturn(mockedResults);
 
         final QueryRestrictions<ResourceIdentifier> queryRestrictions = testUtils.buildQueryRestrictions();
@@ -191,6 +193,11 @@ public class HodDocumentServiceTest {
         documentsService.getStateToken(testUtils.buildQueryRestrictions(), 30, false);
     }
 
+    @Test(expected = NotImplementedException.class)
+    public void getStateTokenAndResultCount() throws HodErrorException {
+        documentsService.getStateTokenAndResultCount(testUtils.buildQueryRestrictions(), 30, false);
+    }
+
     private void validateResults(final Documents<HodSearchResult> results) {
         assertNotNull(results);
         assertThat(results.getDocuments(), not(empty()));
@@ -201,10 +208,20 @@ public class HodDocumentServiceTest {
     }
 
     @SuppressWarnings("CastToConcreteClass")
-    private Documents<HodSearchResult> mockResults() {
-        final HodSearchResult resultWithIndexInQuery = new HodSearchResult.Builder().setIndex(testUtils.getDatabases().get(0).getName()).build();
-        final HodSearchResult resultWithPublicIndex = new HodSearchResult.Builder().setIndex(ResourceIdentifier.NEWS_ENG.getName()).build();
-        final HodSearchResult resultWithPrivateIndex = new HodSearchResult.Builder().setIndex("SomeIndex").build();
-        return new Documents<>(Arrays.asList(resultWithIndexInQuery, resultWithPublicIndex, resultWithPrivateIndex), 3, null, null, null, null);
+    private QueryResults<HodSearchResult> mockResults() {
+        final HodSearchResult resultWithIndexInQuery = new HodSearchResult.Builder()
+                .setIndex(testUtils.getDatabases().get(0).getName())
+                .build();
+        final HodSearchResult resultWithPublicIndex = new HodSearchResult.Builder()
+                .setIndex(ResourceIdentifier.NEWS_ENG.getName())
+                .build();
+        final HodSearchResult resultWithPrivateIndex = new HodSearchResult.Builder()
+                .setIndex("SomeIndex")
+                .build();
+        final HodWarning warning = new HodWarning.Builder()
+                .setCode(40003)
+                .setDetails("There is an error setting the promotions. File is missing.")
+                .build();
+        return new QueryResults<>(Arrays.asList(resultWithIndexInQuery, resultWithPublicIndex, resultWithPrivateIndex), 3, null, null, null, Collections.singletonList(warning));
     }
 }
