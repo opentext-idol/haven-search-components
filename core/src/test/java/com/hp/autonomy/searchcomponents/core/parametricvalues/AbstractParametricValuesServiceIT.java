4121495ad9d8ae91b8311680f8fd9612aaee74d6
Matthew Gordon <matthew.gordon2@hpe.com>
2017-02-07 12:20:26 +0000

    [FIND-838] Added an integration test for paginating parametric values.
    [rev: alex.scown]

--- core/src/test/java/com/hp/autonomy/searchcomponents/core/parametricvalues/AbstractParametricValuesServiceIT.java
+++ core/src/test/java/com/hp/autonomy/searchcomponents/core/parametricvalues/AbstractParametricValuesServiceIT.java
@@ -27,6 +27,7 @@ import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -39,22 +40,32 @@ import static org.hamcrest.Matchers.*;
 @RunWith(SpringRunner.class)
 @JsonTest
 @AutoConfigureJsonTesters(enabled = false)
-public abstract class AbstractParametricValuesServiceIT<R extends ParametricRequest<Q>, F extends FieldsRequest, FB extends FieldsRequestBuilder<F, ?>, Q extends QueryRestrictions<?>, E extends Exception> {
+public abstract class AbstractParametricValuesServiceIT<
+        R extends ParametricRequest<Q>,
+        F extends FieldsRequest,
+        FB extends FieldsRequestBuilder<F, ?>,
+        Q extends QueryRestrictions<S>,
+        S extends Serializable,
+        E extends Exception
+> {
     @Autowired
     private FieldsService<F, E> fieldsService;
     @Autowired
-    private ParametricValuesService<R, Q, E> parametricValuesService;
-    @Autowired
-    protected ObjectFactory<FB> fieldsRequestBuilderFactory;
+    private ObjectFactory<FB> fieldsRequestBuilderFactory;
     @Autowired
     private ObjectFactory<ParametricRequestBuilder<R, Q, ?>> parametricRequestBuilderFactory;
-    @Autowired
-    private TagNameFactory tagNameFactory;
 
+    @Autowired
+    protected TagNameFactory tagNameFactory;
+    @Autowired
+    protected ParametricValuesService<R, Q, E> parametricValuesService;
     @Autowired
     protected TestUtils<Q> testUtils;
 
     protected abstract FieldsRequestBuilder<F, ?> fieldsRequestParams(final FB fieldsRequestBuilder);
+
+    // Find a parametric field with 2 or more values for the parametric request used in these tests
+    protected abstract TagName determinePaginatableField();
 
     @Test
     public void getParametricValues() throws E {
@@ -93,6 +104,33 @@ public abstract class AbstractParametricValuesServiceIT<R extends ParametricRequ
         }
     }
 
+    @Test
+    public void getPaginatedParametricValues() throws E {
+        final TagName fieldName = determinePaginatableField();
+
+        final R page1Request = parametricRequestBuilderFactory.getObject()
+                .queryRestrictions(testUtils.buildQueryRestrictions())
+                .fieldName(fieldName)
+                .start(1)
+                .maxValues(1)
+                .build();
+
+        final Set<QueryTagInfo> page1 = parametricValuesService.getParametricValues(page1Request);
+
+        final R page2Request = parametricRequestBuilderFactory.getObject()
+                .queryRestrictions(testUtils.buildQueryRestrictions())
+                .fieldName(fieldName)
+                .start(2)
+                .maxValues(2)
+                .build();
+
+        final Set<QueryTagInfo> page2 = parametricValuesService.getParametricValues(page2Request);
+
+        assertThat(page1, hasSize(1));
+        assertThat(page2, hasSize(1));
+        assertThat(page1, not(equalTo(page2)));
+    }
+
     private R createNumericParametricRequest() {
         return parametricRequestBuilderFactory.getObject()
                 .fieldName(tagNameFactory.buildTagName(ParametricValuesService.AUTN_DATE_FIELD))
@@ -101,7 +139,7 @@ public abstract class AbstractParametricValuesServiceIT<R extends ParametricRequ
                 .build();
     }
 
-    private R createParametricRequest() throws E {
+    protected R createParametricRequest() throws E {
         final FieldsRequestBuilder<F, ?> fieldsRequestBuilder = fieldsRequestParams(fieldsRequestBuilderFactory.getObject());
         final List<TagName> fields = fieldsService.getFields(fieldsRequestBuilder.build(), FieldTypeParam.Parametric).get(FieldTypeParam.Parametric);
 
--- hod/src/test/java/com/hp/autonomy/searchcomponents/hod/parametricvalues/HodParametricValuesServiceIT.java
+++ hod/src/test/java/com/hp/autonomy/searchcomponents/hod/parametricvalues/HodParametricValuesServiceIT.java
@@ -5,21 +5,29 @@
 
 package com.hp.autonomy.searchcomponents.hod.parametricvalues;
 
+import com.hp.autonomy.hod.client.api.resource.ResourceName;
 import com.hp.autonomy.hod.client.error.HodErrorException;
 import com.hp.autonomy.searchcomponents.core.parametricvalues.AbstractParametricValuesServiceIT;
 import com.hp.autonomy.searchcomponents.hod.beanconfiguration.HavenSearchHodConfiguration;
 import com.hp.autonomy.searchcomponents.hod.fields.HodFieldsRequest;
 import com.hp.autonomy.searchcomponents.hod.fields.HodFieldsRequestBuilder;
 import com.hp.autonomy.searchcomponents.hod.search.HodQueryRestrictions;
+import com.hp.autonomy.types.requests.idol.actions.tags.TagName;
 import org.apache.commons.lang3.NotImplementedException;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = HavenSearchHodConfiguration.class)
-public class HodParametricValuesServiceIT extends AbstractParametricValuesServiceIT<HodParametricRequest, HodFieldsRequest, HodFieldsRequestBuilder, HodQueryRestrictions, HodErrorException> {
+public class HodParametricValuesServiceIT extends AbstractParametricValuesServiceIT<HodParametricRequest, HodFieldsRequest, HodFieldsRequestBuilder, HodQueryRestrictions, ResourceName, HodErrorException> {
     @Override
     protected HodFieldsRequestBuilder fieldsRequestParams(final HodFieldsRequestBuilder fieldsRequestBuilder) {
         return fieldsRequestBuilder.databases(testUtils.buildQueryRestrictions().getDatabases());
+    }
+
+    @Override
+    protected TagName determinePaginatableField() {
+        // Can't use value details for non-numeric fields on HOD, but we know the tests use wiki_eng
+        return tagNameFactory.buildTagName("WIKIPEDIA_CATEGORY");
     }
 
     @Override
