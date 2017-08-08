c28b3a214e653bc68eaa34ce83d7d96e8e88b8a8
Tung Jin Chew <tung-jin.chew@hpe.com>
2017-08-08 15:16:52 +0100

    Allow specifying a map of arbitrary proxied parameters to AnswerServer (which must be configured on AnswerServer as [Server] AllowedQueryParameters=... ; and requires a version of answer server >= 1296154)

--- idol/src/main/java/com/hp/autonomy/searchcomponents/idol/answer/ask/AskAnswerServerServiceImpl.java
+++ idol/src/main/java/com/hp/autonomy/searchcomponents/idol/answer/ask/AskAnswerServerServiceImpl.java
@@ -16,13 +16,12 @@ import com.hp.autonomy.types.idol.responses.answer.AskResponsedata;
 import com.hp.autonomy.types.requests.idol.actions.answer.AnswerServerActions;
 import com.hp.autonomy.types.requests.idol.actions.answer.params.AskParams;
 import com.hp.autonomy.types.requests.idol.actions.answer.params.AskSortParam;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import static com.hp.autonomy.searchcomponents.idol.answer.ask.AskAnswerServerService.ASK_SERVICE_BEAN_NAME;
 import static com.hp.autonomy.searchcomponents.idol.exceptions.codes.IdolErrorCodes.ANSWER_SERVER;
