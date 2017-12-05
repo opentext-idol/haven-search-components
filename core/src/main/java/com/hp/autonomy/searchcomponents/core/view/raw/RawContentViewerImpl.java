5aa2c06221e12daee5b48e8f58a5b4de82ab3e00
Tung Jin Chew <tung-jin.chew@hpe.com>
2017-12-05 18:08:04 +0000

    (FIND-1495) Make the DRECONTENT view fallback do highlighting.

--- core/src/main/java/com/hp/autonomy/searchcomponents/core/view/raw/RawContentViewerImpl.java
+++ core/src/main/java/com/hp/autonomy/searchcomponents/core/view/raw/RawContentViewerImpl.java
@@ -10,12 +10,42 @@ import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static com.hp.autonomy.searchcomponents.core.view.ViewServerService.HIGHLIGHT_END_TAG;
+import static com.hp.autonomy.searchcomponents.core.view.ViewServerService.HIGHLIGHT_START_TAG;
+
 @Component(RawContentViewer.RAW_CONTENT_VIEWER_BEAN_NAME)
 class RawContentViewerImpl implements RawContentViewer {
     private static final Pattern NEWLINE_PATTERN = Pattern.compile("\n", Pattern.LITERAL);
+    private static final Pattern HIGHLIGHT_PATTERN = Pattern.compile(Pattern.quote(HIGHLIGHT_START_TAG) + "(.*?)" + Pattern.quote(HIGHLIGHT_END_TAG));
+
+    private String escapeHtmlExceptHighlighting(final String input) {
+        final Matcher matcher = HIGHLIGHT_PATTERN.matcher(input);
+
+        int prev = 0;
+        final StringBuilder sb = new StringBuilder();
+
+        while(matcher.find()) {
+            final int start = matcher.start();
+            if (prev < start) {
+                sb.append(StringEscapeUtils.escapeHtml(input.substring(prev, start)));
+            }
+
+            prev = matcher.end();
+
+            sb.append(HIGHLIGHT_START_TAG)
+              .append(StringEscapeUtils.escapeHtml(matcher.group(1)))
+              .append(HIGHLIGHT_END_TAG);
+        }
+
+        if (prev < input.length()) {
+            sb.append(StringEscapeUtils.escapeHtml(input.substring(prev)));
+        }
+
+        return sb.toString();
+    }
 
     private String escapeAndAddLineBreaks(final String input) {
-        return input == null ? "" : NEWLINE_PATTERN.matcher(StringEscapeUtils.escapeHtml(input)).replaceAll(Matcher.quoteReplacement("<br>"));
+        return input == null ? "" : NEWLINE_PATTERN.matcher(escapeHtmlExceptHighlighting(input)).replaceAll(Matcher.quoteReplacement("<br>"));
     }
 
     @Override
