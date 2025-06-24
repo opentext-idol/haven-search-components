6bf426e3fbbb0698711b9315b95ba3b49cc50878
Joseph Lansdowne <jlansdowne@opentext.com>
2025-06-24 12:40:09 +0100

    [US15505] remove unused deployment-related files

--- travis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ ${TRAVIS_BRANCH} == 'master' || ${TRAVIS_BRANCH} == 'develop' ]]
-then
-  echo "Deploying Jar to Maven Central"
-  mvn deploy -DskipTests --settings settings.xml -Prelease
-else
-  echo "Not deploying jar"
-fi
